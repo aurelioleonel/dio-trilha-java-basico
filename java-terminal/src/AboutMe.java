c9fe377094222c52689cb540381687699cb1b500
MarcosAurelio <aurelioleonel@gmail.com>
2023-03-01 23:31:42 -0300

    Adicionando o arquivo AboutMe.java

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        // os argumentos começam com indice 0
+        // String nome = args[0];
+        // String sobrenome = args[1];
+        // int idade = Integer.valueOf(args[2]); // vamos falar sobre Wrappers
+        // double altura = Double.valueOf(args[3]);
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+
+        scanner.close();
+
+        System.out.println("Oi");
+    }
+}
